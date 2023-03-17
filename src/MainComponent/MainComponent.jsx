6b57c2de144bb12cca72ee599017e5ceda8194e7
mamerida <mamerida2018@gmail.com>
2023-03-17 15:35:28 -0300

    add menu with elements and methods

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./MainComponent.module.scss";
+import ButtonComponent from "../FormComponents/ButtonComponent/ButtonComponet";
+import { Link } from "react-router-dom";
 
 export const MainPage = () => {
-    return <h2>This is a main Page</h2>;
+    return (
+        <section className={styles.MenuBoard}>
+            <div className={styles.RouteContainer}>
+                <Link className={styles.option__Title} to={"/"}>
+                    Do Something
+                </Link>
+                <Link className={styles.option} to={"activitiesToDo"}>
+                    Activities to do
+                </Link>
+            </div>
+            <ButtonComponent className={styles.ButtonToMenu}>
+                LogOut
+            </ButtonComponent>
+        </section>
+    );
 };
 
 export default MainPage;
